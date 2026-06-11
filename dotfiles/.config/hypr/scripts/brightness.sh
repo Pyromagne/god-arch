171597b6866e65b1240c1c49d659f21122102df6
Pyromagne <growganoth826@gmail.com>
2026-06-11 15:25:58 +0800

    prevent brightness to go down below 5%

--- dotfiles/.config/hypr/scripts/brightness.sh
+++ dotfiles/.config/hypr/scripts/brightness.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
+MIN=5
+
+get_brightness() {
+  brightnessctl -m | cut -d, -f4 | tr -d '%'
+}
+
+notify_brightness () {
+  BRIGHTNESS=$(get_brightness)
+
+  notify-send -a "brightness" \
+              -u low \
+              -h string:x-canonical-private-synchronous:brightness \
+              -h int:value:"$BRIGHTNESS" \
+              -t 2000 \
+              "$ICON $BRIGHTNESS%"
+}
+
+CUR=$(get_brightness)
+
 case "$1" in
   up)
     brightnessctl set 5%+
     ICON="󰃠"
     ;;
   down)
+    [ "$CUR" -le "$MIN" ] && exit 0
     brightnessctl set 5%-
     ICON="󰃞"
     ;;
 esac
 
-BRIGHTNESS=$(brightnessctl info | grep -oP '\(\K[^%]+')
-
-notify-send -a "brightness" \
-            -u low \
-            -h string:x-canonical-private-synchronous:volume \
-            -h int:value:$BRIGHTNESS \
-            -t 2000 \
-            "$ICON $BRIGHTNESS"
+notify_brightness
